5b1ccf807fbb9173cf1e475bdd01962ae1af61ee
haris-dwi-prakoso <harisdwi.prakoso@gmail.com>
2025-02-20 18:50:56 +0700

    Add comments

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -19,18 +19,26 @@ export class UserService {
     private userRepository: Repository<User>
   ) { }
   async create(createUserDto: CreateUserDto) {
+    // Create and save user entity
     let newUser = await this.userRepository.create(createUserDto);
     let saveUser = await this.userRepository.save(newUser);
+    // Save user entity to cache
     await this.cacheManager.set(`user/${saveUser.id}`, saveUser, 6000);
+    // Update list of users in cache
+    let allUsers = await this.userRepository.find();
+    await this.cacheManager.set('user/all', allUsers, 6000);
     return saveUser;
   }
 
   async findAll() {
+    // Check if list of users exists in cache
     const cacheValue = await this.cacheManager.get('user/all');
     if (cacheValue) return cacheValue;
     else {
+      // Check if database is empty
       let allUsers = await this.userRepository.find();
       if (!allUsers || allUsers.length == 0) {
+        // Get list of users from JSONPlaceholder and populate cache & database with the list
         let response = await firstValueFrom(
           this.httpService.get(`https://jsonplaceholder.typicode.com/users`)
         );
@@ -39,6 +47,7 @@ export class UserService {
         await this.userRepository.insert(createUsers);
         return response.data;
       } else {
+        // Save list of users to cache
         await this.cacheManager.set('user/all', allUsers, 6000);
         return allUsers;
       }
@@ -46,13 +55,17 @@ export class UserService {
   }
 
   async findOne(id: number) {
+    // Check if user data exists in cache
     const cacheValue = await this.cacheManager.get(`user/${id}`);
     if (cacheValue) return cacheValue;
     else {
+      // Get user data from database
       let getUser = await this.userRepository.findOneBy({ id: id });
       if (getUser) {
+        // Save user data to cache if user exists in database
         await this.cacheManager.set(`user/${id}`, getUser, 6000);
-      } else if (id <= 10) {
+      } else if (id > 0 && id <= 10) {
+        // Get user data from JSONPlaceholder and save to cache and database if id is within the range of 1 to 10
         let response = await firstValueFrom(
           this.httpService.get(`https://jsonplaceholder.typicode.com/users/${id}`)
         );
@@ -66,13 +79,16 @@ export class UserService {
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
+    // Append id to update data
     let updateData = { id: id, ...updateUserDto };
     let saveUser = await this.userRepository.save(updateData);
+    // Save user data to cache
     await this.cacheManager.set(`user/${saveUser.id}`, saveUser, 6000);
     return saveUser;
   }
 
   async remove(id: number) {
+    // Wipe user data from cache
     await this.cacheManager.del(`user/${id}`);
     return await this.userRepository.delete(id);
   }
